f883736e5d792430a05f012caa47b378dffb2872
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2024-09-27 10:09:21 -0700

    config database Tech Store

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+mongoose.connect(`mongodb://localhost:27017/techStore`)
+.then(() => {
+    console.log('Database Tech Store is connected');
+
+    app.listen(PORT, () => console.log(`Server listaning on the port: ${PORT}`));
+}).catch((err) => {
+    console.log('DATABASE cannot cannected!');
+})
